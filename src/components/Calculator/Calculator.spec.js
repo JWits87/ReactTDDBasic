8ae724e658ffe7c9d486ce6458091001a4ea570c
Jordan Stroud <confidencejws@icloud.com>
2020-03-16 18:37:01 -0400

    Created Display and Tests

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -13,6 +13,6 @@ describe('Calculator', () => {
     });
 
     it('should render the Display Component', () => {
-        expect(wrapper.containsMatchingElement(<Display />)).toEqual(true);
+        expect(wrapper.containsMatchingElement(<Display displayValue={wrapper.instance().state.displayValue} />)).toEqual(true);
     });
 });
